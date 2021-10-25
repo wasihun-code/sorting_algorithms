9d70cb1eaaf9bd61f44a3800a2db889e4afa0698
wasihun-code <wasihunageru7947@gmail.com>
2021-10-25 23:37:41 +0530

    Fixed: Repetition

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,20 +11,20 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, temp, min;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 2; i++)
 	{
 		min = i;
-
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
-			{
 				min = j;
-			}
 		}
+		if (i != min)
+		{
 			temp = array[i];
 			array[i] = array[min];
 			array[min] = temp;
 			print_array(array, size);
+		}
 	}
 }
