7f151a5b73a35ac011ad086d6f36ea5b41c05201
wasihun-code <wasihunageru7947@gmail.com>
2021-10-25 23:18:38 +0530

    Fix: Betty code error and add selection sort implementation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,17 +9,17 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j, temp;
+	size_t i, j, temp;
 
 	for (j = 0; j < size - 1; j++)
 	{
 		for (i = 0; i < size - 1; i++)
 		{
-			if (array[i] > array[i+1])
+			if (array[i] > array[i + 1])
 			{
 				temp = array[i];
-				array[i] = array[i+1];
-				array[i+1] = temp;
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
 				print_array(array, size);
 			}
 		}
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,4 +9,22 @@
 
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, temp, min;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+	}
 }
